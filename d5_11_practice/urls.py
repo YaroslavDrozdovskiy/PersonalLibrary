8edd7c5045ba77eac7c59fdcf3fa8dc548432772
YaroslavDrozdovskiy <54240910+YaroslavDrozdovskiy@users.noreply.github.com>
2020-08-09 13:08:06 +1000

    uyf

--- d5_11_practice/urls.py
+++ d5_11_practice/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('library/', include('p_library.urls'))
+    path('', include('p_library.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
